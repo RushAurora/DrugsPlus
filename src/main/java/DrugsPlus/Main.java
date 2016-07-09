ac2c01f577a7ba48db57cb6415cc3fd55f7f0c00
Jake <jakehcasto@aol.com>
2016-07-09 17:22:08 -0400

    Inital Commit

---
+++ src/main/java/DrugsPlus/Main.java
@@ -0,0 +1,21 @@
+package DrugsPlus;
+
+import cn.nukkit.plugin.PluginBase;
+import cn.nukkit.utils.TextFormat; 
+import cn.nukkit.utils.Utils; 
+
+import java.io.File; 
+import java.io.IOException; 
+import java.util.LinkedHashMap;
+
+import cn.nukkit.event.player.PlayerItemHeldEvent;
+import cn.nukkit.event.player.PlayerInteractEvent;
+
+/* Copyright (C) ImagicalGamer - All Rights Reserved
+ * Unauthorized copying of this file, via any medium is strictly prohibited
+ * Proprietary and confidential
+ * Written by Jake C <imagicalgamer@outlook.com>, July 2016
+ */
+ 
+ public class Main extends PluginBase implements Listener{
+}
