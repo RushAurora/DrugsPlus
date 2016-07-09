dfa7dc0aee5123b618ba1861c4ce6a6510f29cb8
Jake <jakehcasto@aol.com>
2016-07-09 18:32:33 -0400

    DONE! I THINK...
    Need to test

--- src/main/java/DrugsPlus/Main.java
+++ src/main/java/DrugsPlus/Main.java
@@ -10,10 +10,14 @@ import cn.nukkit.item.Item;
 import cn.nukkit.level.particle.SmokeParticle;
 import cn.nukkit.math.Vector3;
 import cn.nukkit.utils.Utils;
-
-
 import cn.nukkit.event.player.PlayerItemHeldEvent;
 import cn.nukkit.event.player.PlayerInteractEvent;
+
+/* Copyright (C) ImagicalGamer - All Rights Reserved
+ * Unauthorized copying of this file, via any medium is strictly prohibited
+ * Proprietary and confidential
+ * Written by Jake C <imagicalgamer@outlook.com>, July 2016
+ */
  
  public class Main extends PluginBase implements Listener{
   
@@ -49,6 +53,46 @@ import cn.nukkit.event.player.PlayerInteractEvent;
     case 414:
      player.getInventory.setItemInHand(item.setCustomName(TextFormat.RESET  + TextFormat.WHITE + "Steroids");
     break;
+   }
+  }
+  
+  public void onInteract(PlayerInteractEvent event){
+   player = event.getPlayer();
+   item = player.getInventory().getItemInHand();
+   level = player.getLevel();
+   switch(item.getId()){
+    case 296:
+     player.addEffect(Effect.getEffect(9).setDuration(20 * 10).setAmplifier(2));
+		 		player.addEffect(Effect.getEffect(10).setDuration(20 * 5).setAmplifier(2));
+		 		player.addEffect(Effect.getEffect(2).setDuration(20 * 10));
+		 		Item InvItem = new Item(296,0,1);
+	 		 player.getInventory().remove(InvItem);
+		 		player.getLevel().addParticle(new SmokeParticle(new Vector3(player.getX(), player.getY(), player.getZ())));
+		 		player.sendMessage(TextFormat.GREEN + "[DrugsPlus]" + TextFormat.WHITE + " You Smoked Weed!");
+		 	break;
+		 	case 353:
+		 	 player.addEffect(Effect.getEffect(1).setDuration(20 * 10).setAmplifier(2));
+		 		player.addEffect(Effect.getEffect(3).setDuration(20 * 5).setAmplifier(2));
+		 		player.addEffect(Effect.getEffect(8).setDuration(20 * 10));
+		 		Item InvItem = new Item(353,0,1);
+	 		 player.getInventory().remove(InvItem);
+		 		player.sendMessage(TextFormat.GREEN + "[DrugsPlus]" + TextFormat.WHITE + " You Snorted some Cocaine!");
+		 	break;
+		 	case 331:
+		 	 player.addEffect(Effect.getEffect(1).setDuration(20 * 10).setAmplifier(2));
+		 		player.addEffect(Effect.getEffect(8).setDuration(20 * 5).setAmplifier(2));
+		 		player.addEffect(Effect.getEffect(9).setDuration(20 * 10));
+		 		Item InvItem = new Item(331,0,1);
+	 		 player.getInventory().remove(InvItem);
+		 		player.sendMessage(TextFormat.GREEN + "[DrugsPlus]" + TextFormat.WHITE + " You Injected Herion!");
+		 	break;
+		 	case 475:
+		 	 player.addEffect(Effect.getEffect(1).setDuration(20 * 10).setAmplifier(2));
+		 		player.addEffect(Effect.getEffect(9).setDuration(20 * 10));
+		 		Item InvItem = new Item(475,0,1);
+	 		 player.getInventory().remove(InvItem);
+	 		 player.getLevel().addParticle(new SmokeParticle(new Vector3(player.getX(), player.getY(), player.getZ())));
+		 		player.sendMessage(TextFormat.GREEN + "[DrugsPlus]" + TextFormat.WHITE + " You Smoked Spice!");
    }
   }
  }
