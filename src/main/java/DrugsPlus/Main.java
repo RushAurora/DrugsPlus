2428fc4b7ba2bf375f66be5311e2d01b55f78839
RushAurora <RushAuroraPE@gmail.com>
2016-07-09 22:54:48 +0100

    Update Main.java

--- src/main/java/DrugsPlus/Main.java
+++ src/main/java/DrugsPlus/Main.java
@@ -1,37 +1,45 @@
-package DrugsPlus;
+package Drugs;
 
-import cn.nukkit.plugin.PluginBase;
-import cn.nukkit.utils.TextFormat; 
-import cn.nukkit.utils.Utils; 
-
-import java.io.File; 
-import java.io.IOException; 
-import java.util.LinkedHashMap;
-
+import cn.nukkit.Player;
 import cn.nukkit.event.player.PlayerItemHeldEvent;
-import cn.nukkit.event.player.PlayerInteractEvent;
+import cn.nukkit.item.Item;
+import cn.nukkit.level.particle.SmokeParticle;
+import cn.nukkit.math.Vector3;
+import cn.nukkit.plugin.PluginBase;
+import cn.nukkit.potion.Effect;
+import cn.nukkit.utils.TextFormat;
 
-/* Copyright (C) ImagicalGamer - All Rights Reserved
- * Unauthorized copying of this file, via any medium is strictly prohibited
- * Proprietary and confidential
- * Written by Jake C <imagicalgamer@outlook.com>, July 2016
+
+/**
+ * Created by FuryTacticz & iMagicalGamer on 08.07.2016.
  */
- 
- public class Main extends PluginBase implements Listener{
-  
-  @Override 
-  public void onEnable(){
-   this.getLogger().info(TextFormat.GREEN + "Enabled!");
-  }
-  
-  @Override
-  public boolean onHeld(PlayerItemHeldEvent event){
-   player = event.getPlayer();
-   item = player.getInventory().getItemInHand();
-   switch(item.getId()){
-    case 296:
-     player.getInventory.setItemInHand(item.setCustomName(TextFormat.RESET  + TextFormat.GREEN + "Weed");
-    break;
-   }
-  }
- }
+public class Drugs extends PluginBase {
+	@Override 
+	  public void onEnable() { 
+	  this.getLogger().info(TextFormat.GREEN  + "Nicks by FuryTacticz & iMagicalGamer Enabled!"); 
+	} 
+
+	public void onHeld(PlayerItemHeldEvent event){
+		Player player = event.getPlayer();
+		Item item = player.getInventory().getItemInHand();
+		
+		 switch(item.getId()){
+		 	case 296:
+		 		player.getInventory().setItemInHand(item.setCustomName(TextFormat.RESET  + TextFormat.GREEN + "Weed"));
+		 		player.addEffect(Effect.getEffect(9).setDuration(20 * 10).setAmplifier(2));
+		 		player.addEffect(Effect.getEffect(10).setDuration(20 * 5).setAmplifier(2));
+		 		player.addEffect(Effect.getEffect(2).setDuration(20 * 10));
+		 		
+		 		Item InvItem = new Item(296,0,1);
+		 		
+		 		player.getInventory().remove(InvItem);
+		 		
+		 		player.getLevel().addParticle(new SmokeParticle(new Vector3(player.getX(), player.getY(), player.getZ())));
+		 		
+		 		player.sendMessage(TextFormat.GREEN + "[DrugsPlus]" + TextFormat.WHITE + " You Smoked Weed!");
+		 		
+		 		break;
+		 }
+	}
+}
+
