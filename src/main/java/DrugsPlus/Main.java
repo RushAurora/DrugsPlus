c0cddfaf909085c7a17994c36d032d153733f1d7
Jake <jakehcasto@aol.com>
2016-07-09 18:08:58 -0400

    Small fix

--- src/main/java/DrugsPlus/Main.java
+++ src/main/java/DrugsPlus/Main.java
@@ -1,21 +1,19 @@
 package DrugsPlus;
 
 import cn.nukkit.plugin.PluginBase;
-import cn.nukkit.utils.TextFormat; 
-import cn.nukkit.utils.Utils; 
+import cn.nukkit.potion.Effect;
+import cn.nukkit.utils.TextFormat;
+import cn.nukkit.Player;
+import cn.nukkit.event.Listener;
+import cn.nukkit.event.player.PlayerItemHeldEvent;
+import cn.nukkit.item.Item;
+import cn.nukkit.level.particle.SmokeParticle;
+import cn.nukkit.math.Vector3;
+import cn.nukkit.utils.Utils;
 
-import java.io.File; 
-import java.io.IOException; 
-import java.util.LinkedHashMap;
 
 import cn.nukkit.event.player.PlayerItemHeldEvent;
 import cn.nukkit.event.player.PlayerInteractEvent;
-
-/* Copyright (C) ImagicalGamer - All Rights Reserved
- * Unauthorized copying of this file, via any medium is strictly prohibited
- * Proprietary and confidential
- * Written by Jake C <imagicalgamer@outlook.com>, July 2016
- */
  
  public class Main extends PluginBase implements Listener{
   
@@ -24,8 +22,9 @@ import cn.nukkit.event.player.PlayerInteractEvent;
    this.getLogger().info(TextFormat.GREEN + "Enabled!");
   }
   
+  //naming the items so it shows their drug names PlayerInteractEvent is for using the drugs
   @Override
-  public boolean onHeld(PlayerItemHeldEvent event){
+  public void onHeld(PlayerItemHeldEvent event){
    player = event.getPlayer();
    item = player.getInventory().getItemInHand();
    switch(item.getId()){
